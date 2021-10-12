a053955e0fcf33e0278c769ada5d2394979ce4a1
Opilane TTHK <opilane@tthk.ee>
2021-10-12 09:18:48 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Tere tulemast!");
+            string eesnimi = Console.ReadLine();
+            Console.WriteLine("Tere, " + eesnimi);
+            if (eesnimi.ToLower() == "Juku")
+            {
+                Console.WriteLine("Tule minu juurde külla! Lähme kinno! Kui vana sa oled?{ }", eesnimi);
+                int vanus = int.Parse(Console.ReadLine());
+                if (vanus < 0 || vanus >= 120)
+                {
+                    Console.WriteLine("Viga andmetega!");
+
+                }
+                else if (vanus <= 6)
+                {
+                    Console.WriteLine("Tasuta pilet!");
+                }
+                else if (vanus > 6 && vanus <= 14)
+                {
+                    Console.WriteLine("Lastepilet!");
+                }
+                else if (vanus <= 15)
+                {
+                    Console.WriteLine("Täispilet!");
+                }
+                else if (vanus > 65)
+                {
+                    Console.WriteLine("sooduspilet!");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Täna mind kodus pole!");
+            }
+
+
+            int arv1 = int.Parse(Console.ReadLine());
+            int arv2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Arvude {0} ja {1} korrutis võrdub {2}", arv1, arv2, arv1 * arv2);
+
+
+
+            Console.WriteLine("Tere, sisestage esimene nimi");
+            string eesnimi = Console.ReadLine();
+            Console.WriteLine("Tere, sisestage teine nimi");
+            string eesnimi2 = Console.ReadLine();
+            if (eesnimi.ToLower() == "Daria" && eesnimi2.ToLower() == "natasha")
+            {
+                Console.WriteLine("Te olete pinginaabrid");
+            }
+
+            
+            int sum = 0;
+            int mult = 1;
+            for(int i=0; i<5; i++)
+            {
+                Console.WriteLine("Palun sisesta number: ");
+                int smt = Convert.ToInt32(Console.ReadLine());
+                sum += smt;
+                mult = smt * mult;
+            }
+            double avg = sum / 5;
+            Console.WriteLine($"Nums AVG={avg}\nNums SUM={sum}\nNums multiplited={mult}");*/
+
+            string text;
+            do
+            {
+                Console.WriteLine("Buy the elephant!");
+            }
+
+
+           
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
+       
